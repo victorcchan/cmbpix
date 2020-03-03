e17951757dd1d80ecfabf5a0ccd3cecfc7b391ff
Victor <chan@astro.utoronto.ca>
2020-03-03 01:59:36 -0500

    Fix issue #1

--- cmbpix/utils.py
+++ cmbpix/utils.py
@@ -6,7 +6,7 @@ def patches(ind, nsideIN, nsideOUT):
     Return the indices of the subpixels of resolution nsideOUT
     of the patch at index ind of resolution nsideIN.
     """
-    if nsideOUT//2 == nsideIN: # Base case
+    if nsideOUT/2 == nsideIN: # Base case
         ipix = []
         for i in np.arange(4) + 4*hp.ring2nest(nsideIN, ind):
             ipix.append(hp.nest2ring(nsideOUT, i))
