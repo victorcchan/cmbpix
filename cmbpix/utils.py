f23c86901b58d9fbdfe71ff75d1687565b555af8
Victor <chan@astro.utoronto.ca>
2020-03-03 01:52:01 -0500

    Add patches function

--- cmbpix/utils.py
+++ cmbpix/utils.py
@@ -1,2 +1,19 @@
 import numpy as np
 import healpy as hp
+
+def patches(ind, nsideIN, nsideOUT):
+    """
+    Return the indices of the subpixels of resolution nsideOUT
+    of the patch at index ind of resolution nsideIN.
+    """
+    if nsideOUT//2 == nsideIN: # Base case
+        ipix = []
+        for i in np.arange(4) + 4*hp.ring2nest(nsideIN, ind):
+            ipix.append(hp.nest2ring(nsideOUT, i))
+        return ipix
+    else:
+        ipix = []
+        for i in np.arange(4) + 4*hp.ring2nest(nsideIN, ind):
+            ipix = ipix + patches(hp.nest2ring(nsideIN*2, i), nsideIN*2, \
+                                  nsideOUT)
+        return ipix
