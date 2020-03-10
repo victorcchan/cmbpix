5ba89ef1411f797341855c19419876ebefb927b6
Victor <chan@astro.utoronto.ca>
2020-03-10 01:39:38 -0400

    Add history to LensingEstimator doc

--- cmbpix/lensing.py
+++ cmbpix/lensing.py
@@ -23,16 +23,10 @@ class LensingEstimator():
 			The input CMB map in HEALPix format.
 		NSIDE_in: int
 			The NSIDE resolution of the input map.
+
+		History
+		-------
+		2020-03-10: First implementation.
 		"""
 		self.map_in = cmbmap
 		self.NSIDE_in = hp.npix2nside(self.map_in.size)
-
-	def EvaluateGradient(self):
-		"""Evaluate the 2D gradient of the CMB map.
-		
-		Evaluate the 2D gradient of the original input map across 
-		the sky. 
-		"""
-		alm = hp.map2alm(self.map_in)
-		m, self.map_dtheta, self.map_dphi = hp.alm2map_der1(alm, self.NSIDE_in)
-		del m, alm
