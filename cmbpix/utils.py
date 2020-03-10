062f9672f64aabf133a55a06b889699b8834c667
Victor <chan@astro.utoronto.ca>
2020-03-10 00:59:32 -0400

    Add docstring for patches

--- cmbpix/utils.py
+++ cmbpix/utils.py
@@ -2,9 +2,37 @@ import numpy as np
 import healpy as hp
 
 def patches(ind, nsideIN, nsideOUT):
-    """
-    Return the indices of the subpixels of resolution nsideOUT
-    of the patch at index ind of resolution nsideIN.
+    """Daughter pixel indices in a low resolution HEALPix patch.
+
+    Return HEALPix indices for all pixels of a higher resolution map 
+    contained inside the pixel(s) of a lower resolution map. The input
+    indices must be in the RING ordering scheme.
+
+    Parameters
+    ----------
+    ind: int or array of ints
+    	Index of the parent HEALPix patch(es).
+    nsideIN: int
+    	NSIDE resolution of the parent HEALPix patch(es).
+    nsideOUT: int
+    	NSIDE resolution of the daughter HEALPix pixels.
+
+    Returns
+    -------
+	ipix: list of int
+		Indices of all pixels contained with the parent patch(es).
+
+	References
+	----------
+	[1] G{\'o}rski, K. M., et al. 2005.
+	"HEALPix: A Framework for High-Resolution Discretization and Fast 
+	Analysis of Data Distributed on the Sphere." 
+	The Astrophysical Journal 622, 759.
+
+	History
+	-------
+	2020-03-02: First added to package.
+	2020-03-10: Docstring added.
     """
     if nsideOUT/2 == nsideIN: # Base case
         ipix = []
