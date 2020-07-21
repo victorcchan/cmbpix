79b86f3d8cb608919b8b611f30f519944bd224da
Victor Chan <chan@astro.utoronto.ca>
2020-07-21 15:42:52 -0400

    Fix indentation

--- cmbpix/utils.py
+++ cmbpix/utils.py
@@ -37,81 +37,81 @@ def ell2ang(ell):
     return 2 * 10800. / (ell + 1)
 
 def patches(ind, NSIDEin, NSIDEout, nest=False):
-	"""Daughter pixel indices in a low resolution HEALPix patch.
+    """Daughter pixel indices in a low resolution HEALPix patch.
 
-	Return HEALPix indices for all pixels of a higher resolution map 
-	contained inside the pixel(s) of a lower resolution map. Output pixels 
-	are always in the RING ordering scheme.
+    Return HEALPix indices for all pixels of a higher resolution map 
+    contained inside the pixel(s) of a lower resolution map. Output pixels 
+    are always in the RING ordering scheme.
 
-	Parameters
-	----------
-	ind: int or array of ints
-		Index of the parent HEALPix patch(es).
-	NSIDEin: int
-		NSIDE resolution of the parent HEALPix patch(es).
-	NSIDEout: int
-		NSIDE resolution of the daughter HEALPix pixels.
-	nest: bool, optional
-		If True, assume ``ind`` are given in NESTED pixel ordering. 
-		Otherwise, assume RING ordering. Default: False.
+    Parameters
+    ----------
+    ind: int or array of ints
+        Index of the parent HEALPix patch(es).
+    NSIDEin: int
+        NSIDE resolution of the parent HEALPix patch(es).
+    NSIDEout: int
+        NSIDE resolution of the daughter HEALPix pixels.
+    nest: bool, optional
+        If True, assume ``ind`` are given in NESTED pixel ordering. 
+        Otherwise, assume RING ordering. Default: False.
 
-	Returns
-	-------
-	ipix: 1d-array of int
-		Indices of all pixels contained with the parent patch(es). Output 
-		is always in RING ordering.
-	
-	"""
-	if NSIDEout/2 == NSIDEin: # Base case
-		inds = np.array(ind)
-		if nest:
-			return hp.nest2ring(NSIDEout, np.tile(np.arange(4), inds.size) + \
-				4*inds.repeat(4))
-		else:
-			return hp.nest2ring(NSIDEout, np.tile(np.arange(4), inds.size) + \
-								4*hp.ring2nest(NSIDEin, inds).repeat(4))
-	else:
-		inds = np.array(ind)
-		s = inds.size
-		if nest:
-			ipix = np.tile(np.arange(4), s) + 4*inds.repeat(4)
-		else:
-			ipix = np.tile(np.arange(4), s) + \
-				4*hp.ring2nest(NSIDEin, inds).repeat(4)
-		return np.concatenate((patches(ipix[:s], NSIDEin*2, NSIDEout, True), 
-								patches(ipix[s:2*s], NSIDEin*2, \
-									NSIDEout, True), 
-								patches(ipix[2*s:3*s], NSIDEin*2, \
-									NSIDEout, True), 
-								patches(ipix[3*s:], NSIDEin*2, \
-									NSIDEout, True), 
-								))
+    Returns
+    -------
+    ipix: 1d-array of int
+        Indices of all pixels contained with the parent patch(es). Output 
+        is always in RING ordering.
+
+    """
+        if NSIDEout/2 == NSIDEin: # Base case
+            inds = np.array(ind)
+        if nest:
+            return hp.nest2ring(NSIDEout, np.tile(np.arange(4), inds.size) + \
+                4*inds.repeat(4))
+        else:
+            return hp.nest2ring(NSIDEout, np.tile(np.arange(4), inds.size) + \
+                4*hp.ring2nest(NSIDEin, inds).repeat(4))
+        else:
+            inds = np.array(ind)
+            s = inds.size
+            if nest:
+                ipix = np.tile(np.arange(4), s) + 4*inds.repeat(4)
+            else:
+                ipix = np.tile(np.arange(4), s) + \
+                    4*hp.ring2nest(NSIDEin, inds).repeat(4)
+        return np.concatenate((patches(ipix[:s], NSIDEin*2, NSIDEout, True), 
+                                patches(ipix[s:2*s], NSIDEin*2, \
+                                    NSIDEout, True), 
+                                patches(ipix[2*s:3*s], NSIDEin*2, \
+                                    NSIDEout, True), 
+                                patches(ipix[3*s:], NSIDEin*2, \
+                                    NSIDEout, True), 
+                                ))
 
 def filter_map(map_in, fl):
-	"""Apply a filter to the given map.
+    """Apply a filter to the given map.
 
-	Return a filtered version of HEALPix map by applying f_ell in 
-	alm space.
+    Return a filtered version of HEALPix map by applying f_ell in 
+    alm space.
 
-	Parameters
-	----------
-	map_in: 1d-array
-		HEALPix map (of the CMB) to be filtered.
-	fl: 1d-array
-		The filter to be applied. Should have values for 0 < ell < 
-		3*NSIDE - 1.
+    Parameters
+    ----------
+    map_in: 1d-array
+        HEALPix map (of the CMB) to be filtered.
+    fl: 1d-array
+        The filter to be applied. Should have values for 0 < ell < 
+        3*NSIDE - 1.
 
-	Returns
-	-------
-	map_out: 1d-array
-		HEALPix map of the original, but filtered with fl.
+    Returns
+    -------
+    map_out: 1d-array
+        HEALPix map of the original, but filtered with fl.
 
-	"""
-	NSIDE_in = hp.npix2nside(len(map_in))
-	alms = hp.map2alm(map_in)
-	alms = hp.almxfl(alms, fl)
-	map_out = hp.alm2map(alms, NSIDE_in)
-	return map_out
+    """
+    NSIDE_in = hp.npix2nside(len(map_in))
+    alms = hp.map2alm(map_in)
+    alms = hp.almxfl(alms, fl)
+    map_out = hp.alm2map(alms, NSIDE_in)
+    return map_out
 
 def numgradient_flat(map_in):
     """Return the gradient maps of map_in.
@@ -193,16 +193,16 @@ def shift2d(map_in):
 
 def _lin(x, *p):
     """A generic line function.
-    
+
     Return y = mx + b using p[0] = b, p[1] = m.
-    
+
     Parameters
     ----------
     x: real, array of reals
         Point(s) at which to evaluate function.
     p: array of size 2
         The linear coefficients of the function: p = [b, m].
-    
+
     Returns
     -------
     y: real, array of reals
