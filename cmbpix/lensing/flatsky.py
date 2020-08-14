41a630c24fa0d70ee6923c294f7603af9af3b17a
victorcchan <chan@astro.utoronto.ca>
2020-08-14 14:55:54 -0400

    Fix bugs in fitting/patch statistics

--- cmbpix/lensing/flatsky.py
+++ cmbpix/lensing/flatsky.py
@@ -2,6 +2,27 @@ import numpy as np
 from pixell import enmap, utils
 from cmbpix.utils import *
 from cmbpix.lensing.estimator import LensingEstimator
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+def _lin(x, *p):
+    """A generic line function.
+
+    Return y = mx + b using p[0] = b, p[1] = m.
+
+    Parameters
+    ----------
+    x: real, array of reals
+        Point(s) at which to evaluate function.
+    p: array of size 2
+        The linear coefficients of the function: p = [b, m].
+
+    Returns
+    -------
+    y: real, array of reals
+        The linear function output(s) evaluated at x.
+    """
+    return p[0] + p[1]*x
 
 class FlatSkyLens(LensingEstimator):
     """Estimator for small scale lensing in a flat sky CMB map.
@@ -57,7 +78,7 @@ class FlatSkyLens(LensingEstimator):
         self._ly, self._lx = self.map_in.lmap()
         self._lmod = self.map_in.modlmap()
 
-    def gather_patches():
+    def gather_patches(self):
         """Assemble patch statistics relevant to lensing at small scales.
 
         Compute the small scale (ell > lmin) temperature power at different 
@@ -65,8 +86,9 @@ class FlatSkyLens(LensingEstimator):
         background temperature gradient (ell < ldT).
 
         """
+        self._edge = 0 # Not throwing away edge pixels
         m_fft = enmap.fft(self.map_in)
-        hp = np.zeros(map_in.shape)
+        hp = np.zeros(self.map_in.shape)
         hp[np.where((self._lmod > self.lmin) & (self._lmod < self.lmax))] = 1.
         self._Tss = enmap.ifft(m_fft * hp)
         self._dTy, self._dTx = gradient_flat(self.map_in, self.ldT)
@@ -74,7 +96,7 @@ class FlatSkyLens(LensingEstimator):
         # Scale geometry for lower res map of patches
         pshp, pwcs = enmap.scale_geometry(self.map_in.shape, 
                                             self.map_in.wcs, 1./self._p)
-        self._Tpatch = enmap.zeros(pshp, pwcs)
+        self._T2patch = enmap.zeros(pshp, pwcs)
         self._dTxpatch = enmap.zeros(pshp, pwcs)
         self._dTypatch = enmap.zeros(pshp, pwcs)
         Trs = self._Tss[:pshp[-2]*self._p,
@@ -86,11 +108,13 @@ class FlatSkyLens(LensingEstimator):
         dTyrs = self._dTy[:pshp[-2]*self._p,
                             :pshp[-1]*self._p].reshape([pshp[-2], self._p, 
                                                         pshp[-1], self._p])
-        self._Tpatch[:,:] = np.var(Trs, axis=(1,3))
+        self._T2patch[:,:] = np.var(Trs, axis=(1,3))
         self._dTypatch[:,:] = np.mean(dTyrs, axis=(1,3))
         self._dTxpatch[:,:] = np.mean(dTxrs, axis=(1,3))
+        self._dT2patch = self._dTxpatch**2 + self._dTypatch**2
 
-    def gather_patches_cos():
+
+    def gather_patches_cos(self):
         """Assemble patch statistics for small scale lensing with cos filter.
         
         Compute the small scale (ell > 3000) temperature power at different 
@@ -113,12 +137,12 @@ class FlatSkyLens(LensingEstimator):
         # Scale geometry for lower res map of patches
         pshp, pwcs = enmap.scale_geometry(self.map_in.shape, 
                                             self.map_in.wcs, 1./self._p)
-        self._Tpatch = enmap.zeros(pshp, pwcs)
+        self._T2patch = enmap.zeros(pshp, pwcs)
         self._dTxpatch = enmap.zeros(pshp, pwcs)
         self._dTypatch = enmap.zeros(pshp, pwcs)
         self._T_sub = np.zeros((pshp[-2], pshp[-1], p, p))
-        for i in range(self._Tpatch.shape[-2]):
-            for j in range(self._Tpatch.shape[-1]):
+        for i in range(self._T2patch.shape[-2]):
+            for j in range(self._T2patch.shape[-1]):
                 self._dTypatch[i,j] = np.mean(self._dTy[i*p:(i+1)*p, 
                                                         j*p:(j+1)*p])
                 self._dTxpatch[i,j] = np.mean(self._dTx[i*p:(i+1)*p, 
@@ -126,6 +150,7 @@ class FlatSkyLens(LensingEstimator):
                 Tp = self._Tss[i*p:(i+1)*p,j*p:(j+1)*p]
                 lsuby, lsubx = Tp.lmap()
                 lsubmod = Tp.modlmap()
+                lsubmod[0,0] = 1.
                 fl = 1.j*(lsubx*self._dTxpatch[i,j] + \
                             lsuby*self._dTypatch[i,j]) / \
                         (lsubmod * np.sqrt(self._dTxpatch[i,j]**2 + \
@@ -133,9 +158,10 @@ class FlatSkyLens(LensingEstimator):
                 fl[0,0] = 0.
                 self._T_sub[i,j,:,:] = enmap.ifft(enmap.fft(Tp)*fl).real
                 # Throw away pixels with edge effects
-                self._Tpatch[i,j] = np.var(self._T_sub[i,j,5:-5,5:-5])
+                self._T2patch[i,j] = np.var(self._T_sub[i,j,5:-5,5:-5])
+        self._dT2patch = self._dTxpatch**2 + self._dTypatch**2
 
-    def gather_patches_cos2():
+    def gather_patches_cos2(self):
         """Assemble patch statistics for small scale lensing with cos^2 filter.
         
         Compute the small scale (ell > 3000) temperature power at different 
@@ -147,7 +173,7 @@ class FlatSkyLens(LensingEstimator):
             f_\\ell = \\cos^2(\\hat{\\ell}\\cdot\\hat{\\nabla T})
 
         """
-        self._edge = 5 # Edge pixels to throw away
+        self._edge = 3 # Edge pixels to throw away
         p = self._p
         m_fft = enmap.fft(self.map_in)
         hp = np.zeros(self.map_in.shape)
@@ -158,12 +184,12 @@ class FlatSkyLens(LensingEstimator):
         # Scale geometry for lower res map of patches
         pshp, pwcs = enmap.scale_geometry(self.map_in.shape, 
                                             self.map_in.wcs, 1./self._p)
-        self._Tpatch = enmap.zeros(pshp, pwcs)
+        self._T2patch = enmap.zeros(pshp, pwcs)
         self._dTxpatch = enmap.zeros(pshp, pwcs)
         self._dTypatch = enmap.zeros(pshp, pwcs)
         self._T_sub = np.zeros((pshp[-2], pshp[-1], p, p))
-        for i in range(self._Tpatch.shape[-2]):
-            for j in range(self._Tpatch.shape[-1]):
+        for i in range(self._T2patch.shape[-2]):
+            for j in range(self._T2patch.shape[-1]):
                 self._dTypatch[i,j] = np.mean(self._dTy[i*p:(i+1)*p, 
                                                         j*p:(j+1)*p])
                 self._dTxpatch[i,j] = np.mean(self._dTx[i*p:(i+1)*p, 
@@ -171,6 +197,7 @@ class FlatSkyLens(LensingEstimator):
                 Tp = self._Tss[i*p:(i+1)*p,j*p:(j+1)*p]
                 lsuby, lsubx = Tp.lmap()
                 lsubmod = Tp.modlmap()
+                lsubmod[0,0] = 1. # Avoid divide by 0; set fl here to 0 later
                 fl = (lsubx*self._dTxpatch[i,j] + \
                         lsuby*self._dTypatch[i,j])**2 / \
                     (lsubmod * np.sqrt(self._dTxpatch[i,j]**2 + \
@@ -178,4 +205,133 @@ class FlatSkyLens(LensingEstimator):
                 fl[0,0] = 0.
                 self._T_sub[i,j,:,:] = enmap.ifft(enmap.fft(Tp)*fl).real
                 # Throw away pixels with edge effects
-                self._Tpatch[i,j] = np.var(self._T_sub[i,j,5:-5,5:-5])
+                self._T2patch[i,j] = np.var(self._T_sub[i,j,3:-3,3:-3])
+        self._dT2patch = self._dTxpatch**2 + self._dTypatch**2
+
+    def fit_binpts(self, bins=20, plot=False):
+        """
+        
+        """
+        g_ord = np.argsort(self._dT2patch.flatten())
+        dT_ord = self._dT2patch.flatten()[g_ord]
+        T_ord = self._T2patch.flatten()[g_ord]
+        g_bin = np.exp(np.linspace(np.log(dT_ord[0]), 
+                                    np.log(dT_ord[-1]), bins))
+        bin_ranges = []
+        nbin = []
+        for b in range(bins-1):
+            bin_ranges.append([g_bin[b], g_bin[b+1]])
+            nbin.append(g_ord[np.where((dT_ord > g_bin[b]) & 
+                                        (dT_ord < g_bin[b+1]))].size)
+        dT2 = np.zeros((len(bin_ranges), 2))
+        T2 = np.zeros((len(bin_ranges), 2))
+        for b in range(len(bin_ranges)):
+            bin_inds = np.where((dT_ord > bin_ranges[b][0]) & \
+                                (dT_ord < bin_ranges[b][1]))
+            n = dT_ord[bin_inds].size
+            dT2[b,:] = [dT_ord[bin_inds].mean(), \
+                        dT_ord[bin_inds].std() / np.sqrt(n), \
+                       ]
+            T2[b,:] = [T_ord[bin_inds].mean(), \
+                       T_ord[bin_inds].std() / np.sqrt(n), \
+                      ]
+        #gb = np.where(np.isfinite(T2[:,0]))
+        # Good bins that contain data points
+        gb = np.where(np.array(nbin) > 5)
+        popt, pcov = curve_fit(_lin, dT2[gb][1:-1,0], T2[gb][1:-1,0], 
+                                [1, 1e-10], sigma=T2[gb][1 :-1,1])
+
+        self.line = popt
+        self.dline = pcov
+        
+        if plot:
+            plt.figure(figsize=(12,8))
+            plt.plot(dT_ord.flatten(), T_ord.flatten(), '.')
+            plt.errorbar(dT2[gb][:,0], T2[gb][:,0], xerr=dT2[gb][:,1], 
+                            yerr=T2[gb][:,1], fmt='.', c='r')
+            plt.plot(g_bin, _lin(g_bin, *popt), c='C1')
+            plt.xscale('log')
+            plt.xlabel(r"$|\nabla T|^2~[\mu{\rm K}^2/rad^2$]")
+            plt.ylabel(r"$\sigma_T^2~[\mu{\rm K}^2]$")
+            plt.tight_layout()
+            plt.show()
+            plt.close()
+
+    def fit_binerr(self, bins=20, plot=False):
+        """
+        
+        """
+        g_ord = np.argsort(self._dT2patch.flatten())
+        dT_ord = self._dT2patch.flatten()[g_ord]
+        T_ord = self._T2patch.flatten()[g_ord]
+        T_err = np.zeros(T_ord.size)
+        N = T_ord.size // bins # Roughly equal number of points in bins
+        for b in range(bins):
+            T_err[N*b:] = T_ord[N*b:N*(b+1)].std()
+        popt, pcov = curve_fit(_lin, dT_ord, T_ord, 
+                                [1, 1e-10], sigma=T_err)
+        self.line = popt
+        self.dline = pcov
+        
+        if plot:
+            plt.figure(figsize=(12,8))
+            plt.errorbar(dT_ord.flatten(), T_ord.flatten(), yerr=T_err, fmt='.')
+            gs = np.linspace(dT_ord.min(), dT_ord.max(), 200)
+            plt.plot(gs, _lin(gs, *popt), c='C1')
+            plt.xscale('log')
+            plt.xlabel(r"$|\nabla T|^2~[\mu{\rm K}^2/rad^2$]")
+            plt.ylabel(r"$\sigma_T^2~[\mu{\rm K}^2]$")
+            plt.tight_layout()
+            plt.show()
+            plt.close()
+
+    def fit_pts(self, bins=20, plot=False):
+        """
+        
+        """
+        g_ord = np.argsort(self._dT2patch.flatten())
+        dT_ord = self._dT2patch.flatten()[g_ord]
+        T_ord = self._T2patch.flatten()[g_ord]
+        T_err = np.ones(T_ord.size)*T_ord.std()
+        popt, pcov = curve_fit(_lin, dT_ord, T_ord, 
+                                [1, 1e-10], sigma=T_err)
+        self.line = popt
+        self.dline = pcov
+        
+        if plot:
+            plt.figure(figsize=(12,8))
+            plt.errorbar(dT_ord.flatten(), T_ord.flatten(), yerr=T_err, fmt='.')
+            gs = np.linspace(dT_ord.min(), dT_ord.max(), 200)
+            plt.plot(gs, _lin(gs, *popt), c='C1')
+            plt.xscale('log')
+            plt.xlabel(r"$|\nabla T|^2~[\mu{\rm K}^2/rad^2$]")
+            plt.ylabel(r"$\sigma_T^2~[\mu{\rm K}^2]$")
+            plt.tight_layout()
+            plt.show()
+            plt.close()
+
+    def fit_ptserr(self, bins=20, plot=False):
+        """
+        
+        """
+        n = (self._p-self._edge)**2 # Number of pixels/patch
+        g_ord = np.argsort(self._dT2patch.flatten())
+        dT_ord = self._dT2patch.flatten()[g_ord]
+        T_ord = self._T2patch.flatten()[g_ord]
+        T_err = T_ord * np.sqrt(2./(n-1))
+        popt, pcov = curve_fit(_lin, dT_ord, T_ord, 
+                                [1, 1e-10], sigma=T_err)
+        self.line = popt
+        self.dline = pcov
+        
+        if plot:
+            plt.figure(figsize=(12,8))
+            plt.errorbar(dT_ord.flatten(), T_ord.flatten(), yerr=T_err, fmt='.')
+            gs = np.linspace(dT_ord.min(), dT_ord.max(), 200)
+            plt.plot(gs, _lin(gs, *popt), c='C1')
+            plt.xscale('log')
+            plt.xlabel(r"$|\nabla T|^2~[\mu{\rm K}^2/rad^2$]")
+            plt.ylabel(r"$\sigma_T^2~[\mu{\rm K}^2]$")
+            plt.tight_layout()
+            plt.show()
+            plt.close()
