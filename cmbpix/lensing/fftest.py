58cd491c3768006cbf52ec56fa8dbddd85484c7b
Victor Chan <chan@astro.utoronto.ca>
2021-11-02 17:11:07 -0400

    Add 4D integral computation for the theoretical normalization and prediction curves

--- cmbpix/lensing/fftest.py
+++ cmbpix/lensing/fftest.py
@@ -36,13 +36,13 @@ def CalcBias(shape, wcs, ells, ldT, lmin, lmax, lbin, lCls, Nls, w, sg, Clpp, pl
     # shape,wcs = enmap.geometry(shape=(side,side),res=res*putils.arcmin,pos=(0,0))
     # shape, wcs = map_in.shape, map_in.wcs
     lmap = enmap.modlmap(shape, wcs)
-    el = np.arange(0, 3000, 1, dtype=np.float64)
+    el = np.arange(0, ldT, 1, dtype=np.float64)
     prefactor = (s.l1*s.l1-s.Ldl1)*(s.l1*s.l1-s.Ldl1)
     spectra = s.e('uC_T_T_l1')*s.e('uC_T_T_l1')/s.e('tC_T_T_l1') * s.e('uC_T_T_l2')*s.e('uC_T_T_l2')/s.e('tC_T_T_l2')
     AL_integrand = prefactor*spectra
     feed_dict = {}
-    feed_dict['uC_T_T'] = s.interp(el,lCls[:3000])(lmap)
-    feed_dict['tC_T_T'] = s.interp(el,lCls[:3000])(lmap)+(w*np.pi/180./60.)**2./s.gauss_beam(lmap,sg)**2.
+    feed_dict['uC_T_T'] = s.interp(el,lCls[:ldT])(lmap)
+    feed_dict['tC_T_T'] = s.interp(el,lCls[:ldT])(lmap)+(w*np.pi/180./60.)**2./s.gauss_beam(lmap,sg)**2.
     tellmin = 0 ; tellmax = ldT
     xmask = s.mask_kspace(shape,wcs,lmin=tellmin,lmax=tellmax)
     integral = s.integrate(shape,wcs,feed_dict,AL_integrand,xmask=xmask,ymask=xmask,physical_units = False).real * enmap.pixsize(shape,wcs)**0.5 / (np.prod(shape[-2:])**0.5)
@@ -87,11 +87,11 @@ def CalcBias(shape, wcs, ells, ldT, lmin, lmax, lbin, lCls, Nls, w, sg, Clpp, pl
         plt.show()
     return cents, AL1d, BL1d
 
-def CalcExp(shape, wcs, lCls, w, sg, Clpp, lmin, lmax, lbin, plots=False):
+def CalcExp(shape, wcs, lCls, w, sg, Clpp, ldT, lmin, lmax, lbin, plots=False):
     # shape, wcs = map_in.shape, map_in.wcs
     lmap = enmap.modlmap(shape, wcs)
-    ells = np.arange(0.,3000.,1, dtype=np.float64)
-    ctt = lCls[:3000]
+    ells = np.arange(0.,ldT,1, dtype=np.float64)
+    ctt = lCls[:ldT]
     cphiphi = Clpp[lmin:lmax]
     ells_small_scale = np.arange(lmin,lmax,1, dtype=np.float64)
     ctt_small_scale = lCls[lmin:lmax]
@@ -120,9 +120,145 @@ def CalcExp(shape, wcs, lCls, w, sg, Clpp, lmin, lmax, lbin, plots=False):
         plt.show()
     return hat_Phi
 
+def l2integral(l1xv, l1yv, Lv, l2xv, l2yv, ClTTunlensed, ClTTtotal, 
+    Clphiphi = None, l2min = 30, l2max = 3000):
+    
+    #Add axes in order to vectorize integral
+    l1x = np.expand_dims(np.expand_dims(np.expand_dims(l1xv,axis=0),axis=-1),
+        axis=-1)
+    l1y = np.expand_dims(np.expand_dims(np.expand_dims(l1yv,axis=0),axis=-1),
+        axis=-1)
+    L = np.expand_dims(np.expand_dims(np.expand_dims(np.expand_dims(Lv,axis=-1),
+        axis=-1),axis=-1),axis=-1)
+    
+    ## Testing:
+    #print(np.shape(l1x))
+    #print(np.shape(l1y))
+    #print(np.shape(L))
+    
+    l2dotl2minusl1 = l2xv*(l2xv-l1x)+l2yv*(l2yv-l1y)
+    Lminusl2dotl1minusl2 = (L-l2xv)*(l1x-l2xv) + (-l2yv)*(l1y-l2yv)
+    l2dotl2minusL = l2xv*(l2xv-L)+l2yv*l2yv
+    modl2 = np.sqrt(l2xv**2+l2yv**2)
+    modLminusl2 = np.sqrt((L-l2xv)**2 + l2yv**2)
+    modl1minusl2 = np.sqrt((l1x-l2xv)**2 + (l1y-l2yv)**2)
+    
+    maskl2 = np.ones(np.shape(modl2),dtype = int)
+    maskl2[np.where(modl2 < l2min)] = 0
+    maskl2[np.where(modl2 > l2max)] = 0
+
+    maskLminusl2 = np.ones(np.shape(modLminusl2),dtype = int)
+    maskLminusl2[np.where(modLminusl2 < l2min)] = 0
+    maskLminusl2[np.where(modLminusl2 > l2max)] = 0
+    
+    hfilter = maskl2*ClTTunlensed[np.rint(modl2).astype(int)]/ClTTtotal[np.rint(modl2).astype(int)] \
+            * maskLminusl2*ClTTunlensed[np.rint(modLminusl2).astype(int)]/ClTTtotal[np.rint(modLminusl2).astype(int)]
+    
+    integrand = l2dotl2minusl1*Lminusl2dotl1minusl2*l2dotl2minusL \
+                * 2. * hfilter * ClTTunlensed[np.rint(modl2).astype(int)] * ClTTunlensed[np.rint(modLminusl2).astype(int)]
+    if Clphiphi is not None:
+        integrand *= Clphiphi[np.rint(modl1minusl2).astype(int)]
+    
+    deltal2x = l2xv[0,1]-l2xv[0,0]
+    deltal2y = l2yv[1,0]-l2yv[0,0]
+    
+    integrand*= deltal2x*deltal2y/((2*np.pi)**2)
+    
+    ## Testing:
+    #print(np.shape(integrand))
+    
+    return np.sum(integrand, axis=(-2,-1))
+
+def l1integral(l1xv, l1yv, l1y, Lv, l2xv, l2yv, ClTTunlensed, ClTTtotal, 
+    Clphiphi = None, l1min = 6000, l1max = 8000, l2min = 30, l2max = 3000):
+
+    #Add axes in order to vectorize integral
+    L = np.expand_dims(np.expand_dims(Lv,axis=-1),axis=-1)
+    
+    ## Testing:
+    #print(np.shape(l1x))
+    #print(np.shape(l1y))
+    #print(np.shape(L))
+    
+    l1dotl1minusL = l1xv*(l1xv-L)+l1yv*l1yv
+    modl1 = np.sqrt(l1xv**2+l1y**2)
+    modLminusl1 = np.sqrt((L-l1xv)**2 + l1yv**2)
+    
+    maskl1 = np.ones(np.shape(modl1),dtype = int)
+    maskl1[np.where(modl1 < l1min)] = 0
+    maskl1[np.where(modl1 > l1max)] = 0
+
+    maskLminusl1 = np.ones(np.shape(modLminusl1),dtype = int)
+    maskLminusl1[np.where(modLminusl1 < l1min)] = 0
+    maskLminusl1[np.where(modLminusl1 > l1max)] = 0
+    
+    gfilter = maskl1*1./ClTTtotal[np.rint(modl1).astype(int)] \
+            * maskLminusl1*1./ClTTtotal[np.rint(modLminusl1).astype(int)]
+    
+    integrand = l1dotl1minusL * gfilter 
+    
+    integrand *= l2integral(l1xv = l1xv, 
+                   l1yv = l1yv, 
+                   Lv = Lv, 
+                   l2xv = l2xv, 
+                   l2yv = l2yv, 
+                   ClTTunlensed = ClTTunlensed, 
+                   ClTTtotal = ClTTtotal, 
+                   Clphiphi = Clphiphi, 
+                   l2min = l2min, 
+                   l2max = l2max)
+   
+    deltal1x = l1xv[0,1]-l1xv[0,0]
+    deltal1y = l1yv[1,0]-l1yv[0,0]
+    
+    integrand*= deltal1x*deltal1y/((2*np.pi)**2)
+    
+    ## Testing:
+    #print(np.shape(integrand))
+    
+    return np.sum(integrand, axis=(-2,-1))
+
+def CalcBiasExp(shape, wcs, uCls, tCls, Clpp, l1min, l1max, l2min, l2max, lbin):
+    nl1x, nl1y = (200, 200)
+    l1x = np.linspace(-l1max, l1max, nl1x)
+    l1y = np.linspace(-l1max, l1max, nl1y)
+    l1xv, l1yv = np.meshgrid(l1x, l1y, sparse=True)  # make sparse output arrays
+
+    ## Turnover of Phi(L) seems to depend on choice of sampling here
+    ## Increase for better precision at cost of speed and memory usage
+    nl2x, nl2y = (40, 40)
+    l2x = np.linspace(-l2max, l2max, nl2x)
+    l2y = np.linspace(-l2max, l2max, nl2y)
+    l2xv, l2yv = np.meshgrid(l2x, l2y, sparse=True)  # make sparse output arrays
+
+    # nL = l2max // lbins
+    # Lmax = l2max
+    # Lmin = l2min
+    # Lv = np.linspace(Lmin, Lmax, nL)
+    Lv = lbin
+
+
+    ## Do each L individually to save on memory usage
+    ## Loop is a bit slower than vectorized calculation, but memory savings is factor of nL
+
+    AL = np.zeros(np.shape(Lv)[0])
+    Phi = np.zeros(np.shape(Lv)[0])
+
+    for iL, LL in enumerate(Lv):
+        AL[iL] = 1./l1integral(l1xv, l1yv, l1y, [LL], l2xv, l2yv, 
+            uCls, tCls, Clphiphi = None, 
+            l1min = l1min, l1max = l1max, l2min = l2min, l2max = l2max)
+        Phi[iL] = l1integral(l1xv, l1yv, l1y, [LL], l2xv, l2yv, 
+            uCls, tCls, Clphiphi = Clpp, 
+            l1min = l1min, l1max = l1max, l2min = l2min, l2max = l2max)*AL[iL]
+
+    return Lv, AL, Phi
+
 def FFTest(map_in, ldT=3000, lmin=6000, lmax=10000, lbins=50, 
-           lCls=None, Nls=None, Clpp=None, w=0., sg=0., 
+           uCls=None, lCls=None, Nls=None, Clpp=None, w=0., sg=0., 
            apply_bias=False, plots=None, show_horn=False):
+    if uCls is None:
+        uCls = np.ones(lmax)
     if lCls is None:
         lCls = np.ones(lmax)
     ell = np.arange(len(lCls), dtype=np.float64)
@@ -141,16 +277,32 @@ def FFTest(map_in, ldT=3000, lmin=6000, lmax=10000, lbins=50,
     binner = bin2D(lmap,bins)
     cents, p1d = binner.bin(p2d)
     if apply_bias:
-        abplot = False
+        c, AL, Phi = CalcBiasExp(shape, wcs, uCls, lCls+Nls, Clpp, lmin, lmax, 0, ldT, cents[cents<ldT])
         if plots == 'all':
-            abplot = True
-        c, AL, BL = CalcBias(shape, wcs, ell, ldT, lmin, lmax, bins[bins<=3000], 
-                 lCls, Nls, w, sg, Clpp, plots=abplot)
-    if Clpp is not None:
-        phiplot = False
-        if plots == 'all':
-            phiplot = True
-        Phi = CalcExp(shape, wcs, lCls, w, sg, Clpp, lmin, lmax, bins[bins<=3000], plots=phiplot)
+            plt.figure(figsize=(8,6))
+            plt.semilogy(c, AL)
+            plt.xlabel(r'$L$')
+            plt.ylabel(r'$A(L)$')
+            plt.tight_layout()
+            plt.show()
+            plt.close()
+            plt.figure(figsize=(8,6))
+            plt.semilogy(c, Phi)
+            plt.xlabel(r'$L$')
+            plt.ylabel(r'$\Psi(L)$')
+            plt.tight_layout()
+            plt.show()
+            plt.close()    
+    #     abplot = False
+    #     if plots == 'all':
+    #         abplot = True
+    #     c, AL, BL = CalcBias(shape, wcs, ell, ldT, lmin, lmax, bins[bins<=ldT], 
+    #              lCls, Nls, w, sg, Clpp, plots=abplot)
+    # if Clpp is not None:
+    #     phiplot = False
+    #     if plots == 'all':
+    #         phiplot = True
+    #     Phi = CalcExp(shape, wcs, lCls, w, sg, Clpp, ldT, lmin, lmax, bins[bins<=ldT], plots=phiplot)
     if plots:
         plt.figure(figsize=(8,6))
         plt.plot(cents, p1d)
@@ -160,18 +312,18 @@ def FFTest(map_in, ldT=3000, lmin=6000, lmax=10000, lbins=50,
         plt.tight_layout()
         plt.show()
         plt.figure(figsize=(8,6))
-        plt.plot(cents[cents<3000], p1d[cents<3000] * AL)
+        plt.plot(cents[cents<ldT], p1d[cents<ldT] * AL)
         plt.axhline(0, c='k', ls=':')
         if Clpp is not None:
             plt.plot(c, Phi, c='k', ls=':')
             if show_horn:
-                plt.fill_between(cents[cents<3000], ex-2*np.sqrt(AL), 
+                plt.fill_between(cents[cents<ldT], ex-2*np.sqrt(AL), 
                                  ex+2*np.sqrt(AL), alpha=0.5)
         plt.xlabel(r"$L$")
         plt.ylabel(r"$C_L^{GK}$")
         plt.tight_layout()
         plt.show()
     if apply_bias and Clpp is not None:
-        return cents, p1d, AL, BL, Phi
+        return cents, p1d, AL, Phi
     else:
         return cents, p1d
