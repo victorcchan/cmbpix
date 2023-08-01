81951e7772ec7daf534a70d8dfeacf7107c19491
Victor Chan <chan@astro.utoronto.ca>
2023-08-01 14:02:45 -0400

    Add SCALE theory computation that utilizes Monte Carlo integration.

--- cmbpix/lensing/SCALE_c.pyx
+++ cmbpix/lensing/SCALE_c.pyx
@@ -1,5 +1,9 @@
 cimport cython
 from cython.parallel import prange
+from libc.stdlib cimport rand, srand, RAND_MAX
+from libc.time cimport time
+import numpy as np
+cimport numpy as np
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -76,4 +80,76 @@ def Psi_and_A_cy(int L, double[:] ClTTunlensed, double[:] ClTTtotal, double[:] C
     # Results need to be scaled by (2\pi)^4
     
     return integral, AL
-                    
+
+def Psi_and_A_cy_mc(int L, double[:] ClTTunlensed, double[:] ClTTtotal, double[:] Clphiphi, \
+                 int l1min, int l1max, int l2min, int l2max, int n_samples, int dl=1):
+    
+    cdef int l1x
+    cdef int l1y
+    cdef int l2x
+    cdef int l2y
+    
+    cdef int l1dotl1minusL
+    cdef int l2dotl2minusl1
+    cdef int Lminusl2dotl1minusl2
+    cdef int l2dotl2minusL
+    
+    cdef int modl1
+    cdef int modLminusl1
+    cdef int modl2
+    cdef int modLminusl2
+    cdef int modl1minusl2
+    
+    cdef int j
+    
+    cdef double integrand = 0.
+    cdef double integral = 0.
+    cdef double AL = 0.
+    
+    cdef np.ndarray[np.int_t,
+                    ndim=2,
+                    negative_indices=False,
+                    mode='c'] l1_vals = np.random.randint(-l1max/dl, l1max/dl, (n_samples, 2))*dl
+    cdef np.ndarray[np.int_t,
+                    ndim=2,
+                    negative_indices=False,
+                    mode='c'] l2_vals = np.random.randint(-l2max/dl, l2max/dl, (n_samples, 2))*dl
+    
+    for j in prange(n_samples, nogil = True):
+        l1x = l1_vals[j,0]
+        l1y = l1_vals[j,1]
+        
+        l2x = l2_vals[j,0]
+        l2y = l2_vals[j,1]
+        modl1 = <int> ((l1x*l1x+l1y*l1y)**(0.5))
+        modLminusl1 = <int> (((L-l1x)*(L-l1x) + l1y*l1y)**(0.5))
+        if (modl1 < l1min or modl1 > l1max):
+            continue
+        elif (modLminusl1 < l1min or modLminusl1 > l1max):
+            continue
+        else:
+            modl2 = <int> ((l2x*l2x+l2y*l2y)**(0.5))
+            modLminusl2 = <int> (((L-l2x)*(L-l2x) + l2y*l2y)**(0.5))
+            if (modl2 < l2min or modl2 > l2max):
+                continue
+            elif (modLminusl2 < l2min or modLminusl2 > l2max):
+                continue
+            else:
+                l1dotl1minusL = l1x*(l1x-L) + l1y*(l1y)
+                l2dotl2minusl1 = l2x*(l2x-l1x) + l2y*(l2y-l1y)
+                Lminusl2dotl1minusl2 = (L-l2x)*(l1x-l2x) + (-l2y)*(l1y-l2y)
+                l2dotl2minusL = l2x*(l2x-L) + l2y*(l2y)
+                modl1minusl2 = <int> (((l1x-l2x)*(l1x-l2x) + (l1y-l2y)*(l1y-l2y))**(0.5))
+                integrand = 2. \
+                    * 1./(ClTTtotal[modl1] * ClTTtotal[modLminusl1]) \
+                    * (ClTTunlensed[modl2]*ClTTunlensed[modl2]/ClTTtotal[modl2]) \
+                    * (ClTTunlensed[modLminusl2]*ClTTunlensed[modLminusl2]/ClTTtotal[modLminusl2]) \
+                    * l1dotl1minusL * l2dotl2minusl1 * Lminusl2dotl1minusl2 * l2dotl2minusL
+                AL += integrand
+                integral += integrand*Clphiphi[modl1minusl2]
+    integral *=1.*(2.*l1max)*(2.*l1max)*(2.*l2max)*(2.*l2max)/n_samples
+    AL *=1.*(2.*l1max)*(2.*l1max)*(2.*l2max)*(2.*l2max)/n_samples
+    AL = 1./AL
+    # Results need to be scaled by (2\pi)^4
+    
+    return integral, AL
